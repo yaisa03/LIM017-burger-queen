bbfc59df8752caf152b6265e4a84b88df241218f
yaisa03 <yaisamichellea@gmail.com>
2022-06-15 19:05:07 -0500

    test waiter version 1

---
+++ src/test/App.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from "react-router-dom";
+import App from '../App';
+
+test('renders learn react link', () => {
+  render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>);
+  
+  const inputUser = screen.getByPlaceholderText(/Usuario/i);
+  expect(inputUser).toBeInTheDocument();
+
+  const inputPassword = screen.getByPlaceholderText(/Contraseña/i);
+  expect(inputPassword).toBeInTheDocument();
+});
