e70247be22463b76162a0b02a58a7d6c0d994221
yaisa03 <yaisamichellea@gmail.com>
2022-06-16 11:55:08 -0500

    vista Orders

--- src/Components/Orders.jsx
+++ src/Components/Orders.jsx
@@ -1,8 +1,20 @@
 import Header from "./Header";
+import './Orders.css';
 export default function Orders() {
     return (
         <>
-        <><Header /></>
+        <Header />
+        <section className="ordersContainer">
+            <div>
+                <p>Pendientes</p>
+            </div>
+            <div>
+                <p>En Proceso</p>
+            </div>
+            <div>
+                <p>Listas</p>
+            </div>
+        </section>
         </>
     );
 }
